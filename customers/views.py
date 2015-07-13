7b812e844e95abe35583e82bedd546133ce36d12
root <tsmyh@mail.ru>
2015-07-13 11:57:10 +0300

    create customers/urls.py

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Customer, CustomerDetails, CustomerGroups
+from contacts.models import Contact
 
 # Create your views here.
+class CustomerList(ListView):
+	# model = Customer
+
+	queryset = Customer.objects.all()
+	context_objects_name = 'customer_list'
+	template_name = 'customers/customer_list.html'
+
+class CustomerDetail(DetailView):
+
+	# queryset = CustomerDetails.objects.all()
+	context_objects_name = 'customer_details'
+	template_name = 'customers/customer_details.html'
+
+	def get_object(self):
+		return Customer.objects.get(pk=self.args[0])
+

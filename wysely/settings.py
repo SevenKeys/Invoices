1f839505cdf772eb5b4c74d10509cfe960efcc01
Paco the taco <paco@coolbet.com>
2015-07-25 17:33:00 +0300

    fixing once again static files of heroku

--- wysely/settings.py
+++ wysely/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
+#
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
 

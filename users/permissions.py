9d7bc509d582f947071598ade03ed2502919e4a4
Paco the taco <paco@coolbet.com>
2015-07-25 12:32:29 +0300

    small changes to force the deploy in heroku with multi buildpack

--- users/permissions.py
+++ users/permissions.py
@@ -1,6 +1,4 @@
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from users.models import User
 
 
 # mixin so that only registered users can have access
@@ -10,13 +8,3 @@ class LoginRequiredMixin(object):
     def as_view(cls, **initkwargs):
         view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
         return login_required(view)
-
-
-# mixin so that users can have access data their company only
-# class CompanyFilterMixin(object):
-# 	def __init__(self):
-# 		self.user = User.objects.get(name=self.request.user)
-# 		self.company = self.user.company
-
-# 	def get_queryset(self):
-# 		return super(CompanyFilterMixin,self).get_queryset().filter(company=self.company)

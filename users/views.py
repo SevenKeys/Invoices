fe5596649d00908ebbdc4fe61b3e16327ff514ef
Paco the taco <paco@coolbet.com>
2015-07-25 11:06:00 +0300

    adding UserProfile when user get registered

--- users/views.py
+++ users/views.py
@@ -9,6 +9,16 @@ from .forms import UserForm
 # from contacts.models import Contact
 from .permissions import LoginRequiredMixin
 
+from django.db.models.signals import post_save
+from users.models import UserProfile
+from django.contrib.auth.models import User
+
+def createUserProfile(sender, instance, **kwargs):
+    """Create a UserProfile object each time a User is created ; and link it.
+    """
+    UserProfile.objects.get_or_create(user=instance)
+
+post_save.connect(createUserProfile, sender=User)
 
 
 class UserList(LoginRequiredMixin, ListView):

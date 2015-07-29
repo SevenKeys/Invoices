6a5c6c5774c660ecd8689906333db3e5d876275f
Francisco Estudillo <festudillobernal@gmail.com>
2015-07-29 19:52:22 +0300

    Adding new styles and applying some in the page

--- wysely/settings.py
+++ wysely/settings.py
@@ -140,7 +140,7 @@ PIPELINE_CSS = {
     # Project libraries.
     'common_styles': {
         'source_filenames': (
-            'bower_components/bootstrap-theme-white-plum/dist/css/bootstrap.css',
+            'css/custom-bootstrap.css',
             'css/main.css',
             'bower_components/gridster/dist/jquery.gridster.min.css',
             # components

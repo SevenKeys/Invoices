f8a20a3239dd68239de914c24b0741b75f59fb32
dmitry <tsmyh@mail.ru>
2015-08-04 17:38:52 +0300

    add css to products form

--- customers/templatetags/add_class.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.simple_tag
-def add_css_class(css_class):
-	return css_class
--- customers/templatetags/extra_customers.py
+++ customers/templatetags/extra_customers.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter
 def fieldtype(obj):
 	return obj.__class__.__name__
+
+
+@register.simple_tag
+def add_css_class(css_class):
+	return css_class

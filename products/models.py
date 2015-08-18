6f2a274432db93e41889c9e410790f8ebadd22b4
dmitry <tsmyh@mail.ru>
2015-08-18 10:27:12 +0300

    fix error with popups

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from companies.models import Company
 
 # class ProductCategory(models.Model):
-#     name = models.CharField()
+#     name = models.CharField(max_length=50)
 
 class ProductGroup(models.Model):
     company = models.ForeignKey(Company)
